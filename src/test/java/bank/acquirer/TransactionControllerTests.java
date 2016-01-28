adfb6e3421b5021fa966dc905b846e249b4a8778
daliborpavicic <dalibor-pavicic@hotmail.com>
2016-01-28 18:35:42 +0100

    Removed dummy test

--- src/test/java/bank/acquirer/TransactionControllerTests.java
+++ src/test/java/bank/acquirer/TransactionControllerTests.java
@@ -1,7 +1,6 @@
 package bank.acquirer;
 
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -11,15 +10,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(BankAppConfig.class)
 @WebAppConfiguration
-public class AcquirerControllerTests {
+public class TransactionControllerTests {
 		
 		@Autowired
 		protected WebApplicationContext context;
@@ -29,14 +23,6 @@ public class AcquirerControllerTests {
 		@Before
 		public void setUp() {
 			this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
-		}
-		
-		@Test
-		public void hello_ShouldReturnHelloMessage() throws Exception {
-			mockMvc.perform(
-					get("/api/bank/acquirer/hello"))
-					.andExpect(status().isOk())
-					.andExpect(jsonPath("$.content", is("Hello from acquirer bank!")));
 		}
 
 }
